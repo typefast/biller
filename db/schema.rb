a243fb9785021deddb1ce55d6be4e1a8c9340451
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-09 12:50:22 +0000

    Add employees table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109124328) do
+ActiveRecord::Schema.define(version: 20160109124857) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160109124328) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+  end
+
+  create_table "employees", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "time_entries", force: true do |t|
