3466adf1128501c619ba3b35c926deb8ba87151d
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-09 12:32:56 +0000

    migrate customers table

--- db/migrate/20160109121356_create_customers.rb
+++ db/migrate/20160109121356_create_customers.rb
@@ -1,7 +1,8 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
-
+      t.string :name, :about
+      t.integer :balance
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160109121356) do
+
+  create_table "customers", force: true do |t|
+    t.string   "name"
+    t.string   "about"
+    t.integer  "balance"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
