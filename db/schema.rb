603020bcf76fc6beb7d039574589215e8bdd0789
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-09 12:46:53 +0000

    create time entries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109123432) do
+ActiveRecord::Schema.define(version: 20160109124328) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160109123432) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+  end
+
+  create_table "time_entries", force: true do |t|
+    t.float    "time"
+    t.integer  "customer_id"
+    t.integer  "employee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
