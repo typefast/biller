1e477905a3c988b3ba1d8d2164f63c6e367850b2
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-09 12:40:02 +0000

    Add email column to customers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109121356) do
+ActiveRecord::Schema.define(version: 20160109123432) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160109121356) do
     t.integer  "balance"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
