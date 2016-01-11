98a3fc59dc33385ed941bb416e6d642dd5745785
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-11 10:36:05 +0000

    rename time_entries account_entries

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111095709) do
+ActiveRecord::Schema.define(version: 20160111103244) do
+
+  create_table "account_entries", force: true do |t|
+    t.float    "time"
+    t.integer  "customer_id"
+    t.integer  "employee_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "accounts", force: true do |t|
     t.string   "type"
     t.string   "name"
     t.string   "email"
     t.string   "about"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "time_entries", force: true do |t|
-    t.float    "time"
-    t.integer  "customer_id"
-    t.integer  "employee_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
