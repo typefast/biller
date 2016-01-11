043bfff477a811721c81cc139691a0b9dd6094f8
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-11 10:02:19 +0000

    remove old tables customers, employees

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111095426) do
+ActiveRecord::Schema.define(version: 20160111095709) do
 
   create_table "accounts", force: true do |t|
     t.string   "type"
     t.string   "name"
     t.string   "email"
     t.string   "about"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "customers", force: true do |t|
-    t.string   "name"
-    t.string   "about"
-    t.integer  "balance"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "email"
-  end
-
-  create_table "employees", force: true do |t|
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
