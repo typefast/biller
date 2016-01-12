55510cef164613c04d6d93f00832203b66e62ba0
Harry Oliver <someonecancode@yahoo.co.uk>
2016-01-12 14:35:39 +0000

    Add methods

--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -2,7 +2,13 @@ class TimeEntry < AccountEntry
   belongs_to :employee
   validates :time, numericality: {greater_than: 0, less_than: 24}
   
-  def cost
-    time * 34
+  before_create :calculate_amount!
+  
+  def self.per_hour
+    34
+  end
+  
+  def calculate_amount!
+    self.amount = TimeEntry.per_hour * self.time
   end
 end
